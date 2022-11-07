7a62e81bc630c4b147c02de9ec6e10215c06122d
M4RT4Z4K <116658532+M4RT4Z4K@users.noreply.github.com>
2022-11-07 20:24:50 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -15,6 +15,14 @@ public:
 	~ResourceManager() { delete X; }
 
 	double get() { return X->get(); }
+	
+		ResourceManager& operator=(const ResourceManager& M)
+	{
+		delete this->X;
+		X = new Resource{};
+		*X = *M.X;
+		return *this;
+	}
 
 private:
 	Resource* X;
